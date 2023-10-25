d569a7b9efeb0e6f0667c4a6c2632d8c6ff65850
Allan Rodrigues <allan.rodrigues14@hotmail.com>
2023-10-25 16:26:20 -0300

    Hey-Teacher-9: PEST - Create Question Test

--- tests/Feature/CreateQuestionTest.php
+++ tests/Feature/CreateQuestionTest.php
@@ -1,10 +1,10 @@
 <?php
 
 it('should be able to create a new question bigger than 255 characters', function () {
-});
+})->todo();
 
 it('should check if ends with question mark?', function () {
-});
+})->todo();
 
 it('should have at least 10 characters', function () {
-});
+})->todo();
