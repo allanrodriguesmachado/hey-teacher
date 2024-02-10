3cc0bcb91c44dc51c2d600e9843e707630f73f64
Allan Rodrigues Machado <allan.rodrigues14@hotmail.com>
2024-02-10 10:01:15 -0300

    Instaling pint and setting up config

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,15 +10,15 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-
-
     protected $fillable = [
         'name',
         'email',
@@ -42,6 +42,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
+        'password'          => 'hashed',
     ];
 }
