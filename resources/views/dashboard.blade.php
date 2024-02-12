c7cd0cbf3f1202a189db6095467adcce6a3b984f
Allan Rodrigues Machado <allan.rodrigues14@hotmail.com>
2024-02-12 15:53:28 -0300

    HEY-9: Implemented Flowbite theme, question form with DB saving, save/reset buttons, and pEST message display integration.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,11 +7,35 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+            <form action="{{route('question.store')}}" method="post">
+                @csrf
+                <div class="mb-4">
+                    <label for="question" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        Sua mensagem
+                    </label>
+                    <textarea id="question" rows="4" name="question"
+                              class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                              placeholder="Escreva aqui sua mensagem...">{{old('question')}}</textarea>
+                    @error('question')
+                    <span class="text-red-400">{{$message}}</span>
+                    @enderror
+
                 </div>
-            </div>
+
+               <div class="flex justify-end items-center">
+                   <button type="reset"
+                           class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                       Cancelar
+                   </button>
+
+                   <button type="submit"
+                           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300
+                        font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700
+                         focus:outline-none dark:focus:ring-blue-800">
+                       Salvar
+                   </button>
+               </div>
+            </form>
         </div>
     </div>
 </x-app-layout>
